0c9bb10feefb1fb88761ff3b8fa1f05af1a818b8
Eduardo Siqueira <53954835+DukaSiqueira@users.noreply.github.com>
2024-05-28 22:34:31 -0300

    Separating styling into specific files and using the method of separating components into folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import Header from './components/Header.jsx';
+import Header from './components/Header/Header.jsx';
 import { CORE_CONCEPTS } from './data.js';
-import CoreConept from './components/CoreConcept.jsx';
+import CoreConept from './components/CoreConcept/CoreConcept.jsx';
 
 function App() {
   return (
