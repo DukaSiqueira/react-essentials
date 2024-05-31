df634f02f35c66677574da494638403e4966bff1
Eduardo Siqueira <53954835+DukaSiqueira@users.noreply.github.com>
2024-05-31 16:34:15 -0300

    Rendering Content Conditionally

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 // React Hook
-import { useState } from 'react';
+import { useState } from "react";
 
-import Header from './components/Header/Header.jsx';
-import { CORE_CONCEPTS, EXAMPLES } from './data.js';
-import CoreConept from './components/CoreConcept/CoreConcept.jsx';
-import TapButton from './components/TapButton/TapButton.jsx';
+import Header from "./components/Header/Header.jsx";
+import { CORE_CONCEPTS, EXAMPLES } from "./data.js";
+import CoreConept from "./components/CoreConcept/CoreConcept.jsx";
+import TapButton from "./components/TapButton/TapButton.jsx";
 
 function App() {
-  const [ selectedTopic, setSelectedTopic ] = useState('components')
+  const [selectedTopic, setSelectedTopic] = useState();
 
   function handleSelect(selectedButton) {
     setSelectedTopic(selectedButton);
@@ -22,34 +22,38 @@ function App() {
           <h2>Core Concepts</h2>
           <ul>
             {/* Verbose example */}
-            <CoreConept 
+            <CoreConept
               title={CORE_CONCEPTS[0].title}
               description={CORE_CONCEPTS[0].description}
               image={CORE_CONCEPTS[0].image}
             />
             {/* Destructured example */}
-            <CoreConept{...CORE_CONCEPTS[1]} />
-            <CoreConept{...CORE_CONCEPTS[2]} />
-            <CoreConept{...CORE_CONCEPTS[3]} />
+            <CoreConept {...CORE_CONCEPTS[1]} />
+            <CoreConept {...CORE_CONCEPTS[2]} />
+            <CoreConept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TapButton onSelect={() => handleSelect('components')}>Components</TapButton>
-            <TapButton onSelect={() => handleSelect('jsx')}>JSX</TapButton>
-            <TapButton onSelect={() => handleSelect('props')}>Props</TapButton>
-            <TapButton onSelect={() => handleSelect('state')}>State</TapButton>
+            <TapButton onSelect={() => handleSelect("components")}>
+              Components
+            </TapButton>
+            <TapButton onSelect={() => handleSelect("jsx")}>JSX</TapButton>
+            <TapButton onSelect={() => handleSelect("props")}>Props</TapButton>
+            <TapButton onSelect={() => handleSelect("state")}>State</TapButton>
           </menu>
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>
-                {EXAMPLES[selectedTopic].code}
-              </code>
-            </pre>
-          </div>
+          {!selectedTopic ? (
+            <p>Please select a topic.</p>
+          ) : (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>
+          )}
         </section>
       </main>
     </div>
