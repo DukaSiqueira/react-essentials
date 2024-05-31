900794ada23150c5730ea1cb72b8e7d627004415
Eduardo Siqueira <53954835+DukaSiqueira@users.noreply.github.com>
2024-05-31 17:47:02 -0300

    Outputting List Data Dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -21,16 +21,19 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
+            {CORE_CONCEPTS.map((concept) => (
+              <CoreConept key={concept.title} {...concept} />
+            ))}
             {/* Verbose example */}
-            <CoreConept
+            {/* <CoreConept
               title={CORE_CONCEPTS[0].title}
               description={CORE_CONCEPTS[0].description}
               image={CORE_CONCEPTS[0].image}
-            />
+            /> */}
             {/* Destructured example */}
-            <CoreConept {...CORE_CONCEPTS[1]} />
+            {/* <CoreConept {...CORE_CONCEPTS[1]} />
             <CoreConept {...CORE_CONCEPTS[2]} />
-            <CoreConept {...CORE_CONCEPTS[3]} />
+            <CoreConept {...CORE_CONCEPTS[3]} /> */}
           </ul>
         </section>
         <section id="examples">
